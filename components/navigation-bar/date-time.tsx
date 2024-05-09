6b93dc922731035be7eacce8100017e710f2131b
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-05-09 20:13:49 -0300

    add opacity animation to date time

--- components/navigation-bar/date-time.tsx
+++ components/navigation-bar/date-time.tsx
@@ -16,8 +16,8 @@ export const DateAndTime = observer(() => {
   return (
     <div
       className={cn(
-        "justify-center grid gap-0.5 pr-6 transition-[grid-template-columns] duration-200 ease-in-out grid-cols-[0fr]",
-        _ && "grid-cols-[1fr]"
+        "justify-center grid gap-0.5 pr-6 transition-[grid-template-columns,opacity] duration-500 ease-in-out grid-cols-[0fr] opacity-0",
+        _ && "grid-cols-[1fr] opacity-100"
       )}
     >
       <div className="overflow-hidden items-end">
