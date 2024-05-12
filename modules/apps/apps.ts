8a72df198c6d6b73ef82791ce09d29066129eaf5
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-05-12 02:25:34 -0300

    add spend splitter app

--- modules/apps/apps.ts
+++ modules/apps/apps.ts
@@ -1,4 +1,5 @@
 import { Application } from "./app";
 import { dataLoomApp } from "./data-loom/app";
+import { spendSplitterApp } from "./spend-splitter/app";
 
-export const applications: Application[] = [dataLoomApp];
+export const applications: Application[] = [dataLoomApp, spendSplitterApp];
---
+++ modules/apps/spend-splitter/app.ts
@@ -0,0 +1,20 @@
+import { Application } from "../app";
+
+export const spendSplitterApp = {
+  name: "Spend Splitter",
+  icon: "https://sangonz193.github.io/spend-splitter/vite.svg",
+  href: "https://sangonz193.github.io/spend-splitter",
+  // infoWindow: {
+  //   id: "spend-splitter-info",
+  //   name: "Spend Splitter Info",
+  //   icon: {
+  //     type: "url",
+  //     src: "https://sangonz193.github.io/spend-splitter/vite.svg",
+  //   },
+  //   content: {
+  //     type: "component",
+  //     component: DataLoomInfoWindow,
+  //   },
+  //   minSize: { width: 300, height: 400 },
+  // },
+} satisfies Application;
