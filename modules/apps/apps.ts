ab1df5729221cffc53902080ced5108885cbb42d
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-05-21 01:29:39 -0300

    add openfing app

--- modules/apps/apps.ts
+++ modules/apps/apps.ts
@@ -1,5 +1,10 @@
 import { Application } from "./app";
 import { dataLoomApp } from "./data-loom/app";
+import { openfingApp } from "./openfing/app";
 import { spendSplitterApp } from "./spend-splitter/app";
 
-export const applications: Application[] = [dataLoomApp, spendSplitterApp];
+export const applications: Application[] = [
+  dataLoomApp,
+  openfingApp,
+  spendSplitterApp,
+];
---
+++ modules/apps/openfing/app.ts
@@ -0,0 +1,20 @@
+import { Application } from "../app";
+
+export const openfingApp = {
+  name: "OpenFING",
+  icon: "https://openfing.sgonzalez.dev/icon.svg",
+  href: "https://openfing.sgonzalez.dev",
+  // infoWindow: {
+  //   id: "spend-splitter-info",
+  //   name: "Spend Splitter Info",
+  //   icon: {
+  //     type: "url",
+  //     src: "https://sangonz193.github.io/spend-splitter/vite.svg",
+  //   },
+  //   content: {
+  //     type: "component",
+  //     component: DataLoomInfoWindow,
+  //   },
+  //   minSize: { width: 300, height: 400 },
+  // },
+} satisfies Application;
