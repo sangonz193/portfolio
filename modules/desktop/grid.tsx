95a2e788fd8067f763c1170f90ebcfd3eaeb0006
Santiago Gonzalez <37671738+sangonz193@users.noreply.github.com>
2024-05-28 01:01:35 -0300

    fix grid row calculation

--- modules/desktop/grid.tsx
+++ modules/desktop/grid.tsx
@@ -5,11 +5,13 @@ import { applications } from "../apps"
 import { dataLoomApp } from "../apps/data-loom/app"
 import { meApp } from "../apps/me/app"
 import { openfingApp } from "../apps/openfing/app"
+import { safeAreaStore } from "../safe-area/store"
 import { viewportSizeStore } from "../viewport/size-store"
 import { WindowIcon } from "../windows/window-icon"
 
 export const DesktopGrid = observer(() => {
   const { height, width } = viewportSizeStore
+  const { insets } = safeAreaStore
 
   if (!height || !width) {
     return null
@@ -17,12 +19,16 @@ export const DesktopGrid = observer(() => {
 
   const cellSize = 110
   const gap = 4
-  const rows = Math.floor((height - gap) / (cellSize + gap))
-  const columns = Math.floor((width - gap) / (cellSize + gap))
+  const rows = Math.floor(
+    (height - gap - insets.bottom - insets.top) / (cellSize + gap),
+  )
+  const columns = Math.floor(
+    (width - gap - insets.right - insets.left) / (cellSize + gap),
+  )
 
   return (
     <div
-      className="absolute inset-0 bottom-[var(--nav-bar-height)] grid grid-flow-col animate-in"
+      className="absolute inset-0 bottom-[var(--nav-bar-height)] grid grid-flow-col bg-red-300 animate-in"
       style={{
         gridTemplateColumns: `repeat(${columns}, ${cellSize}px)`,
         gridTemplateRows: `repeat(${rows}, ${cellSize}px)`,
